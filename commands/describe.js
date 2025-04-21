e7f5db007ded707c610797303f7ff408de9fad59
kephakings <176045599+kephakings@users.noreply.github.com>
2025-04-21 10:35:23 +0300

    Update describe.js

--- commands/describe.js
+++ commands/describe.js
@@ -4,7 +4,7 @@ const {
 const axios = require("axios");
 zokou({
   'nomCom': 'define',
-  'reaction': '♻️',
+  'reaction': '👄',
   'categorie': "Search"
 }, async (_0x2d6773, _0x1778cf, _0x5bcf7e) => {
   const {
